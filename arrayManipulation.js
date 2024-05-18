899d954a2085f9118ef3c27caada9f7697a643cf
Banisco10 <gbaniba@st.ug.edu.gh>
2024-05-18 16:49:26 +0100

    Created arrayManipulation.js file

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,1 +1,11 @@
-
+function processArray(numbers) {
+    const result = [];
+    numbers.forEach(num => {
+        if (num % 2 === 0) {
+            result.push(num * num);
+        } else {
+            result.push(num * 3);
+        }
+    });
+    return result;
+}
